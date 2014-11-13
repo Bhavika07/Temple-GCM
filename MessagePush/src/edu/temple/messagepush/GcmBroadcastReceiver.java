ec21104a5b29cab21506a75bee1e631229ef9662
karlmorris <karlmorris@super3s-imac.cis.temple.edu>
2014-11-13 11:42:21 -0500

    Implmented GcmBroadcastReceiver.

--- MessagePush/src/edu/temple/messagepush/GcmBroadcastReceiver.java
+++ MessagePush/src/edu/temple/messagepush/GcmBroadcastReceiver.java
@@ -1,5 +1,7 @@
 package edu.temple.messagepush;
 
+import android.app.Activity;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v4.content.WakefulBroadcastReceiver;
@@ -7,9 +9,13 @@ import android.support.v4.content.WakefulBroadcastReceiver;
 public class GcmBroadcastReceiver extends WakefulBroadcastReceiver {
 
 	@Override
-	public void onReceive(Context context, Intent intent) {
-		// TODO Auto-generated method stub
-
-	}
+    public void onReceive(Context context, Intent intent) {
+        // Explicitly specify that GcmIntentService will handle the intent.
+        ComponentName comp = new ComponentName(context.getPackageName(),
+                GcmIntentService.class.getName());
+        // Start the service, keeping the device awake while it is launching.
+        startWakefulService(context, (intent.setComponent(comp)));
+        setResultCode(Activity.RESULT_OK);
+    }
 
 }
